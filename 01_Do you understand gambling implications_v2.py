c258dbca843f6ea604fb43614e0fa86e6cb42b73
KarlYapBuller <78565635+KarlYapBuller@users.noreply.github.com>
2021-02-23 13:17:56 +1300

    More efficient do you understand gambling implications yes/no checker

--- 01_Do you understand gambling implications_v2.py
+++ 01_Do you understand gambling implications_v2.py
@@ -4,18 +4,14 @@ while show_gambling_implications.lower() != "xxx":
     #Ask user if they understand the implications of gambling
     show_gambling_implications = input("Do you understand the implications of Gambling?").lower()
     #If user says yes, output 'ask user if they have played before'
-    if show_gambling_implications == "yes":
-        print("Ask user if they have played before?")
-
-    elif show_gambling_implications == "y":
+    if show_gambling_implications == "yes" or show_gambling_implications == "y":
+        show_gambling_implications = "yes"
         print("Ask user if they have played before?")
 
     #If user says no, output 'display gambling implications'
-    elif show_gambling_implications == "no":
+    elif show_gambling_implications == "no" or show_gambling_implications == "n":
+        show_gambling_implications = "no"
         print("Display gambling implications")
-
-    elif show_gambling_implications == "n":
-        print("display gambling implications")
 
     #If user says maybe or any other word 'output <error> answer yes/no'
     else:
